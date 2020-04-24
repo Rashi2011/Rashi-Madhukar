48eb1632302306b161f98986f5b0ba39fa35f2b3
Rashi2011 <54224655+Rashi2011@users.noreply.github.com>
2020-04-24 18:08:08 +0530

    Add files via upload

---
+++ Business Analysis/Business_Analysis.py
@@ -0,0 +1,73 @@
+#PREDICTING WHICH GROUP SHOULD BE ASSIGNED ACCORDING TO THE PERFORMANCE OF EMPLOYEES
+
+#importing libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#import the Dataset
+Dataset = pd.read_csv("DATASET/Business dataset/customerTargeting.csv")
+X = Dataset.iloc[:,:-1].values
+Y = Dataset.iloc[:,70].values
+
+#replace nan with zero and inf with finite numbers
+#np.nan_to_num(X)
+
+#for missing data
+from sklearn.impute import SimpleImputer
+imp = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+X = imp.fit_transform(X)
+'''
+#Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+
+#Splitting into Train And Test 
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size = 1/4,random_state = 0)
+'''
+#Applying PCA
+from sklearn.decomposition import PCA
+pca= PCA(n_components = 2)
+X =pca.fit_transform(X)
+#X_test = pca.transform(X_test)
+
+#K-mean Clustering Technique
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=3,init ='k-means++',max_iter=300,n_init=10,random_state = 0)
+#km.fit(X_train,Y_train)
+
+#Prediction
+km = kmeans.fit_predict(X)
+
+'''
+#Confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,y_pred)
+'''
+#accuracy Score
+from sklearn.metrics import v_measure_score 
+print("v_measure_score",v_measure_score(Y,km))
+
+
+#Visualising the cluster
+plt.scatter(X[Y == 0,0], X[Y == 0,1], s=100, c='red', label = 'cluster 1')
+plt.scatter(X[Y == 1,0], X[Y == 1,1], s=100, c='green', label = 'cluster 2')
+plt.scatter(X[Y == 2,0], X[Y == 2,1], s=100, c='black', label = 'cluster 3')
+#plt.scatter(y_pred.cluster_centers_[:,0], y_pred.cluster_centers_[:,1], s = 300, c='yellow')
+plt.title('K-mean_cluster(real)')
+plt.xlabel('pc1')
+plt.ylabel('pc2')
+plt.show()
+
+#Visualising the cluster
+plt.scatter(X[km == 0,0], X[km == 0,1], s=100, c='red', label = 'cluster 1')
+plt.scatter(X[km == 1,0], X[km == 1,1], s=100, c='green', label = 'cluster 2')
+plt.scatter(X[km == 2,0], X[km == 2,1], s=100, c='black', label = 'cluster 3')
+
+#plt.scatter(y_pred.cluster_centers_[:,0], y_pred.cluster_centers_[:,1], s = 300, c='yellow')
+plt.title('K-mean_cluster')
+plt.xlabel('pc1')
+plt.ylabel('pc2')
+plt.show()
